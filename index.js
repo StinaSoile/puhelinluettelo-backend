f05fda85bb2a8365ed319443effef3147e930bb7
stsosupa <stina.s.s.palomaki@student.jyu.fi>
2024-04-19 14:27:28 +0300

    3.20 done

--- index.js
+++ index.js
@@ -60,18 +60,18 @@ app.get('/api/persons/:id', (request, response, next) => { //next on Expressin j
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
-        response.status(204).end()
+        response.json(result)
     }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Content missing'
-        })
-    }
+    // if (!body.name || !body.number) {
+    //     return response.status(400).json({
+    //         error: 'Content missing'
+    //     })
+    // }
 
     const person = new Person({
         name: body.name,
@@ -82,17 +82,13 @@ app.post('/api/persons', (request, response) => {
         savedPerson => {
             response.json(savedPerson)
         }
-    )
+    ).catch(error => next(error))
 })
 
-// KESKEN!!!! Muut toimii jo
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    Person.findByIdAndUpdate(request.params.id, {
-        name: body.name,
-        number: body.number
-    }, { new: true })
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         }).catch(error => next(error))
@@ -112,6 +108,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
